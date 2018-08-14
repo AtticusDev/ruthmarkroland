b7b26c987c78ba5cc84256571fa41c92f68c05a9
david roland <david.roland@atticuscreative.co.uk>
2018-08-14 19:17:53 +0100

    updating fonts

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<script type="text/javascript" src="//fast.fonts.net/jsapi/8b020e44-c402-41a0-856c-963106a715da.js"></script>
 <?php wp_head(); ?>
 </head>
 
